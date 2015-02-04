73ee17a3aeed5052eb103a11849125e65ce59d91
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-02-04 18:51:19 +0300

    Add `ideaPlugins` key
    Plugins' jars will be added into `unmanagedJars` based on this key's value

--- src/main/scala/Keys.scala
+++ src/main/scala/Keys.scala
@@ -8,11 +8,13 @@ object Keys {
     "Version of Intellij IDEA to build against")
   lazy val ideaBaseDirectory = SettingKey[File]("idea-base-directory",
     "Directory where downloaded IDEA is unpacked")
+  lazy val ideaPlugins = SettingKey[Seq[String]]("idea-plugins",
+    "List of Intellij IDEA plugins this project depends on")
 
   lazy val ideaMainJars = TaskKey[Classpath]("idea-main-jars",
     "Classpath containing main IDEA jars")
-  lazy val ideaCommunityJars = TaskKey[Classpath]("idea-community-jars",
-    "Classpath containing jars of IDEA Community plugins")
+  lazy val ideaPluginJars = TaskKey[Classpath]("idea-plugin-jars",
+    "Classpath containing jars of IDEA plugins")
   lazy val ideaFullJars = TaskKey[Classpath]("idea-full-jars",
     "Concatenation of idea-main-jars and idea-community-jars")
 
@@ -21,18 +23,16 @@ object Keys {
 
   lazy val ideaPluginSettings: Seq[Setting[_]] = Seq(
     ideaBaseDirectory := baseDirectory.value / "idea",
-    ideaMainJars      := (ideaBaseDirectory.value / ideaVersion.value / "lib" * "*.jar").classpath,
+    ideaPlugins       := Seq.empty,
 
-    ideaCommunityJars := {
-      val plugins = Seq("copyright", "gradle", "Groovy", "IntelliLang",
-                        "java-i18n", "android", "maven", "junit", "properties")
-      val dirs = plugins.foldLeft(PathFinder.empty){ (paths, plugin) =>
+    ideaMainJars   := (ideaBaseDirectory.value / ideaVersion.value / "lib" * "*.jar").classpath,
+    ideaPluginJars := {
+      val dirs = ideaPlugins.value.foldLeft(PathFinder.empty){ (paths, plugin) =>
         paths +++ (ideaBaseDirectory.value / ideaVersion.value / "plugins" / plugin / "lib")
       }
       (dirs * (globFilter("*.jar") -- "*asm*.jar")).classpath
     },
-
-    ideaFullJars := ideaMainJars.value ++ ideaCommunityJars.value,
+    ideaFullJars := ideaMainJars.value ++ ideaPluginJars.value,
     unmanagedJars in Compile ++= ideaFullJars.value,
 
     updateIdea <<= (ideaBaseDirectory, ideaVersion, streams).map(Tasks.updateIdea)
