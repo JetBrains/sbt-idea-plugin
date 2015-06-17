3d5a1b7ee2b8680b2588f0447d4ff49dfed35d05
Nikolay.Obedin <Nikolay.Obedin@jetbrains.com>
2015-06-17 17:01:38 +0300

    Refactor "updateIdea" task
    Now it downloads external plugins too

--- src/main/scala/Keys.scala
+++ src/main/scala/Keys.scala
@@ -7,25 +7,35 @@ object Keys {
   lazy val ideaBuild = SettingKey[String](
     "idea-build",
     "Number of Intellij IDEA build to use in project")
-  lazy val ideaBaseDirectory = SettingKey[File](
-    "idea-base-directory",
-    "Directory where downloaded IDEA is unpacked")
+
+  lazy val ideaDownloadDirectory = SettingKey[File](
+    "idea-download-directory",
+    "Directory where IDEA binaries and sources are downloaded")
+
   lazy val ideaInternalPlugins = SettingKey[Seq[String]](
     "idea-internal-plugins",
     "List of names of bundled Intellij IDEA plugins this project depends on")
-  lazy val ideaExternalPlugins = SettingKey[Seq[(String,URI)]](
+
+  lazy val ideaExternalPlugins = SettingKey[Seq[(String,URL)]](
     "idea-external-plugins",
-    "List of (name, URI) pairs of third-party plugins this project depends on")
+    "List of (name, URL) pairs of third-party plugins this project depends on")
+
+  lazy val ideaBaseDirectory = TaskKey[File](
+    "idea-base-directory",
+    "Directory where downloaded IDEA binaries and sources are unpacked")
 
   lazy val ideaMainJars = TaskKey[Classpath](
     "idea-main-jars",
     "Classpath containing main IDEA jars")
+
   lazy val ideaInternalPluginsJars = TaskKey[Classpath](
     "idea-internal-plugins-jars",
     "Classpath containing jars of internal IDEA plugins used in this project")
+
   lazy val ideaExternalPluginsJars = TaskKey[Classpath](
     "idea-external-plugins-jars",
     "Classpath containing jars of external IDEA plugins used in this project")
+
   lazy val ideaFullJars = TaskKey[Classpath](
     "idea-full-jars",
     "Complete classpath of IDEA's and internal and external plugins' jars")
@@ -34,29 +44,32 @@ object Keys {
     "update-idea",
     "Download Intellij IDEA binaries, sources and external plugins for specified build")
 
+
   lazy val ideaPluginSettings: Seq[Setting[_]] = Seq(
-    ideaBaseDirectory := baseDirectory.value / "idea",
+    ideaDownloadDirectory := baseDirectory.value / "idea",
 
     ideaInternalPlugins := Seq.empty,
 
-    ideaMainJars := (ideaBaseDirectory.value / ideaBuild.value / "lib" * "*.jar").classpath,
+    ideaExternalPlugins := Seq.empty,
 
-    ideaInternalPluginsJars <<= (ideaBaseDirectory, ideaBuild, ideaInternalPlugins).map {
-      (baseDir, ideaBuild, pluginsUsed) =>
-        val pluginsBase = baseDir / ideaBuild / "plugins"
-        Tasks.createPluginsClasspath(pluginsBase, pluginsUsed)
+    ideaBaseDirectory <<= (ideaDownloadDirectory, ideaBuild).map {
+      (downloadDir, build) => downloadDir / build
     },
 
-    ideaExternalPluginsJars <<= (ideaBaseDirectory, ideaBuild, ideaExternalPlugins).map {
-      (baseDir, ideaBuild, pluginsUsed) =>
-        val pluginsBase = baseDir / ideaBuild / "externalPlugins"
-        Tasks.createPluginsClasspath(pluginsBase, pluginsUsed.map(_._1))
+    ideaMainJars := (ideaBaseDirectory.value / "lib" * "*.jar").classpath,
+
+    ideaInternalPluginsJars <<= (ideaBaseDirectory, ideaInternalPlugins).map {
+      (baseDir, pluginsUsed) => Tasks.createPluginsClasspath(baseDir / "plugins", pluginsUsed)
+    },
+
+    ideaExternalPluginsJars <<= (ideaBaseDirectory, ideaExternalPlugins).map {
+      (baseDir, pluginsUsed) => Tasks.createPluginsClasspath(baseDir / "externalPlugins", pluginsUsed.map(_._1))
     },
 
     ideaFullJars := ideaMainJars.value ++ ideaInternalPluginsJars.value ++ ideaExternalPluginsJars.value,
 
     unmanagedJars in Compile ++= ideaFullJars.value,
 
-    updateIdea <<= (ideaBaseDirectory, ideaBuild, streams).map(Tasks.updateIdea)
+    updateIdea <<= (ideaBaseDirectory, ideaBuild, ideaExternalPlugins, streams).map(Tasks.updateIdea)
   )
 }
--- src/main/scala/Tasks.scala
+++ src/main/scala/Tasks.scala
@@ -2,41 +2,99 @@ package ideaplugin
 
 import sbt._
 import sbt.Keys._
-import IdeaDownloader._
+import scala.util._
+import scala.xml._
 
 
 object Tasks {
-  def updateIdea(baseDir: File, build: String, streams: TaskStreams): Unit = {
+  import Downloader._
 
-    val buildId = getBuildId(build)
-    val buildMajorNum = build.substring(0, build.indexOf('.'))
-
-    val downloadBaseUrl = s"$TeamcityEndpoint/builds/id:$buildId/artifacts/content"
-    val downloadDir = baseDir / "archives"
-    val unpackDir = baseDir / build
-
+  def updateIdea(baseDir: File, build: String, externalPlugins: Seq[(String, URL)], streams: TaskStreams): Unit = {
+    val externalPluginsDir = baseDir / "externalPlugins"
     IO.createDirectory(baseDir)
-    IO.createDirectory(downloadDir)
-    IO.createDirectory(unpackDir)
-
-    val downloads = Seq(
-      Download(
-        url(s"$downloadBaseUrl/ideaIC-$buildMajorNum.SNAPSHOT.win.zip"),
-        downloadDir / s"ideaIC-$build.zip",
-        Some(unpackDir)),
-      Download(
-        url(s"$downloadBaseUrl/sources.zip"),
-        unpackDir / "sources.zip",
-        None)
-    )
-
-    downloads.foreach(d => downloadArtifact(d, streams))
+    IO.createDirectory(externalPluginsDir)
+    val downloads =
+      createIdeaDownloads(baseDir, build, streams.log) ++
+      createExternalPluginsDownloads(externalPluginsDir, externalPlugins, streams.log)
+    downloads.foreach(d => download(d, streams.log))
   }
 
   def createPluginsClasspath(pluginsBase: File, pluginsUsed: Seq[String]): Classpath = {
-    val pluginsDirs = pluginsUsed.foldLeft(PathFinder.empty){ (paths, plugin) =>
+    val pluginsDirs = pluginsUsed.foldLeft(PathFinder.empty) { (paths, plugin) =>
       paths +++ (pluginsBase / plugin / "lib")
     }
     (pluginsDirs * (globFilter("*.jar") -- "*asm*.jar")).classpath
+  }
+
+  private def createIdeaDownloads(baseDir: File, build: String, log: Logger): Seq[Download] = {
+    import TeamcityDownloader._
+
+    val buildId = getBuildId(build)
+    val buildMajorNum = build.substring(0, build.indexOf('.'))
+    val downloadBaseUrl = s"$TeamcityEndpoint/builds/id:$buildId/artifacts/content"
+
+    Seq(
+      DownloadAndUnpack(
+        url(s"$downloadBaseUrl/ideaIC-$buildMajorNum.SNAPSHOT.win.zip"),
+        baseDir.getParentFile / s"ideaIC-$build.zip",
+        baseDir),
+      DownloadOnly(
+        url(s"$downloadBaseUrl/sources.zip"),
+        baseDir / "sources.zip")
+    )
+  }
+
+  private def createExternalPluginsDownloads(baseDir: File, plugins: Seq[(String, URL)], log: Logger): Seq[Download] =
+    plugins.map { case (pluginName, pluginUrl) =>
+      DownloadAndUnpack(
+        pluginUrl,
+        baseDir / s"$pluginName.zip",
+        baseDir / pluginName
+      )
+    }
+}
+
+private object Downloader {
+  sealed trait Download
+  final case class DownloadOnly(from: URL, to: File) extends Download
+  final case class DownloadAndUnpack(from: URL, to: File, unpackTo: File) extends Download
+
+  def download(d: Download, log: Logger): Unit = d match {
+    case DownloadOnly(from, to) =>
+      download(log, from, to)
+    case DownloadAndUnpack(from, to, unpackTo) =>
+      download(log, from, to)
+      unpack(log, to, unpackTo)
+  }
+
+  private def download(log: Logger, from: URL, to: File): Unit = {
+    if (to.isFile) {
+      log.info(s"$to exists, download aborted")
+    } else {
+      log.info(s"Downloading $from to $to")
+      IO.download(from, to)
+    }
+  }
+
+  private def unpack(log: Logger, from: File, to: File): Unit = {
+    log.info(s"Unpacking $from to $to")
+    IO.unzip(from, to)
+  }
+}
+
+private object TeamcityDownloader {
+  val TeamcityEndpoint = "https://teamcity.jetbrains.com/guestAuth/app/rest"
+
+  def getBuildId(build: String): String = {
+    val metadataUrl = url(s"$TeamcityEndpoint/builds?locator=buildType:(id:bt410),branch:(default:any,name:idea/$build)")
+
+    val buildId = for {
+      metadata <- Try(XML.loadString(IO.readLinesURL(metadataUrl).mkString))
+      buildVal <- Try(metadata \ "build" \\ "@id").map(_.head)
+    } yield buildVal.text
+
+    buildId.recoverWith { case exc =>
+      Failure(new Error(s"Could not retrieve IDEA/$build build ID from $metadataUrl", exc))
+    }.get
   }
 }
