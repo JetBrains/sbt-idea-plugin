057e016644cfb0a229b76a60f1ff36faeb637747
Justin Kaeser <justin.kaeser@jetbrains.com>
2017-11-27 18:17:15 +0300

    rename setting/task keys to new sbt camelCase convention

--- src/main/scala/org/jetbrains/sbtidea/Keys.scala
+++ src/main/scala/org/jetbrains/sbtidea/Keys.scala
@@ -5,64 +5,64 @@ import sbt._
 
 object Keys {
   lazy val ideaBuild = SettingKey[String](
-    "idea-build",
+    "ideaBuild",
     "Number of Intellij IDEA build to use in project")
 
   lazy val ideaDownloadDirectory = SettingKey[File](
-    "idea-download-directory",
+    "ideaDownloadDirectory",
     "Directory where IDEA binaries and sources are downloaded")
 
   lazy val ideaInternalPlugins = SettingKey[Seq[String]](
-    "idea-internal-plugins",
+    "ideaInternalPlugins",
     "List of names of bundled Intellij IDEA plugins this project depends on")
 
   lazy val ideaExternalPlugins = SettingKey[Seq[IdeaPlugin]](
-    "idea-external-plugins",
+    "ideaExternalPlugins",
     "List of third-party plugins this project depends on")
 
   lazy val ideaEdition = SettingKey[IdeaEdition](
-    "idea-edition",
+    "ideaEdition",
     "Edition of Intellij IDEA to use in project")
 
   lazy val ideaDownloadSources = SettingKey[Boolean](
-    "idea-download-sources",
+    "ideaDownloadSources",
     "Flag indicating whether IDEA sources should be downloaded too")
 
   lazy val ideaPublishSettings = SettingKey[PublishSettings](
-    "idea-publish-settings",
+    "ideaPublishSettings",
     "Settings necessary for publishing IDEA plugin to plugins.jetbrains.com")
 
   lazy val ideaPluginFile = TaskKey[File](
-    "idea-plugin-file",
+    "ideaPluginFile",
     "IDEA plugin's file to publish to plugins.jetbrains.com")
 
-  lazy val updateIdea = TaskKey[Unit](
-    "update-idea",
+  lazy val ideaUpdate = TaskKey[Unit](
+    "ideaUpdate",
     "Download Intellij IDEA binaries, sources and external plugins for specified build")
 
-  lazy val publishPlugin = TaskKey[String](
-    "publish-plugin",
+  lazy val ideaPublishPlugin = TaskKey[String](
+    "ideaPublishPlugin",
     "Publish IDEA plugin on plugins.jetbrains.com")
 
 
   lazy val ideaBaseDirectory = TaskKey[File](
-    "idea-base-directory",
+    "ideaBaseDirectory",
     "Directory where downloaded IDEA binaries and sources are unpacked")
 
   lazy val ideaMainJars = TaskKey[Classpath](
-    "idea-main-jars",
+    "ideaMainJars",
     "Classpath containing main IDEA jars")
 
   lazy val ideaInternalPluginsJars = TaskKey[Classpath](
-    "idea-internal-plugins-jars",
+    "ideaInternalPluginsJars",
     "Classpath containing jars of internal IDEA plugins used in this project")
 
   lazy val ideaExternalPluginsJars = TaskKey[Classpath](
-    "idea-external-plugins-jars",
+    "ideaExternalPluginsJars",
     "Classpath containing jars of external IDEA plugins used in this project")
 
   lazy val ideaFullJars = TaskKey[Classpath](
-    "idea-full-jars",
+    "ideaFullJars",
     "Complete classpath of IDEA's and internal and external plugins' jars")
 
 
@@ -95,7 +95,7 @@ object Keys {
 
   lazy val buildSettings: Seq[Setting[_]] = Seq(
     ideaBuild := "LATEST-EAP-SNAPSHOT",
-    ideaDownloadDirectory := baseDirectory.value / "idea",
+    ideaDownloadDirectory := Path.userHome / ".IdeaData" / "sdk",
     ideaEdition := IdeaEdition.Community,
     ideaDownloadSources := true,
     ideaBaseDirectory := ideaDownloadDirectory.value / ideaBuild.value
@@ -114,7 +114,7 @@ object Keys {
     ideaFullJars := ideaMainJars.value ++ ideaInternalPluginsJars.value ++ ideaExternalPluginsJars.value,
     unmanagedJars in Compile ++= ideaFullJars.value,
 
-    updateIdea := tasks.UpdateIdea.apply(
+    ideaUpdate := tasks.UpdateIdea.apply(
       ideaBaseDirectory.value,
       ideaEdition.value,
       ideaBuild.value,
@@ -125,6 +125,6 @@ object Keys {
 
     ideaPluginFile := packageBin.in(Compile).value,
     ideaPublishSettings := PublishSettings("", "", "", None),
-    publishPlugin := tasks.PublishPlugin.apply(ideaPublishSettings.value, ideaPluginFile.value, streams.value)
+    ideaPublishPlugin := tasks.PublishPlugin.apply(ideaPublishSettings.value, ideaPluginFile.value, streams.value)
   )
 }
