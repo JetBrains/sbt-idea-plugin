f5a63ed1b444c85f811edd794a1f1c3e63c6278e
Mikhail Mutcianko <mutcianko.m@gmail.com>
2018-10-25 15:21:27 +0300

    Revert Revert "automatically load the plugin"

--- src/main/scala/org/jetbrains/sbtidea/SbtIdeaPlugin.scala
+++ src/main/scala/org/jetbrains/sbtidea/SbtIdeaPlugin.scala
@@ -6,6 +6,7 @@ import sbt.{AutoPlugin, Setting, plugins}
 abstract class AbstractSbtIdeaPlugin extends AutoPlugin {
   override def requires = plugins.JvmPlugin
   val autoImport: Keys.type = sbtidea.Keys
+  override def trigger = allRequirements
   override def buildSettings: Seq[Setting[_]] = Keys.buildSettings
   override def projectSettings: Seq[Setting[_]] = Keys.projectSettings
 }
