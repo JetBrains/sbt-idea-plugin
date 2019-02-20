7a5587bb98e62e50a362fc05b48c6d8855a927a8
Mikhail Mutcianko <mutcianko.m@gmail.com>
2019-02-20 15:08:28 +0300

    revert auto-loading, again
    oops

--- src/main/scala/org/jetbrains/sbtidea/SbtIdeaPlugin.scala
+++ src/main/scala/org/jetbrains/sbtidea/SbtIdeaPlugin.scala
@@ -4,11 +4,10 @@ import org.jetbrains.sbtidea
 import sbt.{AutoPlugin, Setting, plugins}
 
 abstract class AbstractSbtIdeaPlugin extends AutoPlugin {
-  override def requires = plugins.JvmPlugin
   val autoImport: Keys.type = sbtidea.Keys
-  override def trigger = allRequirements
-  override def globalSettings: Seq[Setting[_]] = Keys.globalSettings
-  override def buildSettings: Seq[Setting[_]] = Keys.buildSettings
+  override def requires = plugins.JvmPlugin
+  override def globalSettings: Seq[Setting[_]]  = Keys.globalSettings
+  override def buildSettings: Seq[Setting[_]]   = Keys.buildSettings
   override def projectSettings: Seq[Setting[_]] = Keys.projectSettings
 }
 
