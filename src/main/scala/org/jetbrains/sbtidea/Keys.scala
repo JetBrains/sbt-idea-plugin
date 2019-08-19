0213dbcbf9223dc19608e431618d6aab7177647b
Mikhail Mutcianko <mutcianko.m@gmail.com>
2019-08-19 20:18:04 +0300

    PackagingMethod.DepsOnly shouldn't require explicit targetPath

--- src/main/scala/org/jetbrains/sbtidea/Keys.scala
+++ src/main/scala/org/jetbrains/sbtidea/Keys.scala
@@ -179,7 +179,7 @@ object Keys {
   object PackagingMethod {
     final case class Skip() extends PackagingMethod
     final case class MergeIntoParent() extends PackagingMethod
-    final case class DepsOnly(targetPath: String) extends PackagingMethod
+    final case class DepsOnly(targetPath: String = "") extends PackagingMethod
     final case class MergeIntoOther(project: Project) extends PackagingMethod
     final case class Standalone(targetPath: String = "", static: Boolean = false) extends PackagingMethod
   }
