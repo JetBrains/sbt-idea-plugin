3c1f51d696137d70911798abeb3ecd6d6e7e354e
Mikhail Mutcianko <mutcianko.m@gmail.com>
2019-09-13 12:36:43 +0300

    add JbIdeaRepoArtifactResolver test

---
+++ ideaSupport/src/test/scala/org/jetbrains/sbtidea/download/JbIdeaRepoArtifactResolverTest.scala
@@ -0,0 +1,33 @@
+package org.jetbrains.sbtidea.download
+
+import org.jetbrains.sbtidea.{ConsoleLogger, PluginLogger}
+import org.scalatest.{FunSuite, Matchers}
+
+import org.jetbrains.sbtidea.Keys._
+
+class JbIdeaRepoArtifactResolverTest extends FunSuite with Matchers with IdeaMock with ConsoleLogger {
+
+  private def createResolver: JBIdeaRepoArtifactResolver = new JBIdeaRepoArtifactResolver {
+    override protected def log: PluginLogger = JbIdeaRepoArtifactResolverTest.this.log
+  }
+
+  test("latest eap is resolved") {
+    val resolver = createResolver
+    val result = resolver.resolveUrlForIdeaBuild(BuildInfo("LATEST-EAP-SNAPSHOT", IdeaEdition.Community))
+    result should not be empty
+  }
+
+  test("latest 192 release is resolved") {
+    val resolver = createResolver
+    val result = resolver.resolveUrlForIdeaBuild(BuildInfo("192.6603.28", IdeaEdition.Community))
+    result should not be empty
+  }
+
+  test("latest 191 release is resolved") {
+    val resolver = createResolver
+    val result = resolver.resolveUrlForIdeaBuild(BuildInfo("191.7479.19", IdeaEdition.Community))
+    result should not be empty
+  }
+
+
+}
