209bcd327f78e1897143b2033cbebe6660ec3737
Mikhail Mutcianko <mutcianko.m@gmail.com>
2019-10-04 18:20:17 +0300

    add plugin xml detector wip #15

--- ideaSupport/src/main/scala/org/jetbrains/sbtidea/xml/PluginXmlDetector.scala
+++ ideaSupport/src/main/scala/org/jetbrains/sbtidea/xml/PluginXmlDetector.scala
@@ -1,7 +1,13 @@
 package org.jetbrains.sbtidea.xml
 
 import java.nio.file.Path
+import org.jetbrains.sbtidea.PathExt
 
-class PluginXmlDetector(targetDir: Path) {
-  def getPluginXml: Option[Path] = ???
+object PluginXmlDetector {
+  def getPluginXml(targetDir: Path): Option[Path] = {
+    val path = targetDir / "META-INF" / "plugin.xml"
+    if (path.toFile.exists())
+      Some(path)
+    else None
+  }
 }
