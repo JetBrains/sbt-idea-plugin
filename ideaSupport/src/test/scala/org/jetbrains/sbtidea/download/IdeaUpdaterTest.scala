6a44601e106d89ec5f667cab81960851550303b0
Mikhail Mutcianko <mutcianko.m@gmail.com>
2019-11-29 15:12:14 +0300

    add ij repo overriding test #42

--- ideaSupport/src/test/scala/org/jetbrains/sbtidea/download/IdeaUpdaterTest.scala
+++ ideaSupport/src/test/scala/org/jetbrains/sbtidea/download/IdeaUpdaterTest.scala
@@ -1,9 +1,9 @@
 package org.jetbrains.sbtidea.download
 
-import org.jetbrains.sbtidea.{CapturingLogger, TmpDirUtils}
+import org.jetbrains.sbtidea.{CapturingLogger, PluginLogger, TmpDirUtils}
 import org.scalatest.{FunSuite, Matchers}
 
-final class IdeaUpdaterTest extends FunSuite with Matchers with TmpDirUtils {
+final class IdeaUpdaterTest extends FunSuite with Matchers with TmpDirUtils with IdeaMock {
 
   import IdeaUpdater._
 
@@ -18,6 +18,19 @@ final class IdeaUpdaterTest extends FunSuite with Matchers with TmpDirUtils {
       capturingLogger.messages shouldBe empty
     } finally {
       sys.props -= DUMB_KEY
+    }
+  }
+
+  test("IJ repo overriding property") {
+    val capturingLogger = new CapturingLogger
+    try {
+      sys.props += IJ_REPO_OVERRIDE -> s"https://127.0.0.1:/non-existing-path"
+      val resolver = new JBIdeaRepoArtifactResolver { override protected def log: PluginLogger = capturingLogger }
+
+      assertThrows[java.net.ConnectException](resolver.resolveUrlForIdeaBuild(IDEA_BUILDINFO))
+      capturingLogger.messages should contain ("Using non-default IntelliJ repository URL: https://127.0.0.1:/non-existing-path")
+    } finally {
+      sys.props -= IJ_REPO_OVERRIDE
     }
   }
 
