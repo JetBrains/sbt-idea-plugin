1cfdf8b0462b72ad05a610155373b4847de89da3
Mikhail Mutcianko <mutcianko.m@gmail.com>
2020-04-01 00:42:05 +0300

    disable sandbox test

--- ideaSupport/src/test/scala/org/jetbrains/sbtidea/download/CommunityIdeaUpdaterTest.scala
+++ ideaSupport/src/test/scala/org/jetbrains/sbtidea/download/CommunityIdeaUpdaterTest.scala
@@ -8,34 +8,34 @@ import org.jetbrains.sbtidea.{ConsoleLogger, pathToPathExt}
 import org.scalatest.{FunSuite, Ignore, Matchers}
 import sbt._
 
-//@Ignore
+@Ignore
 final class CommunityIdeaUpdaterTest extends FunSuite with Matchers with IdeaMock with ConsoleLogger {
 
-  test("IdeaUpdater Integration Test") {
-    val dumbModeKey = "IdeaUpdater.dumbMode"
-    sys.props += dumbModeKey -> "idea"
-//    val tmpDir = Files.createTempDirectory("IdeaUpdaterTest")
-    val tmpDir = Paths.get("/", "tmp", "IdeaUpdater")
-    Files.createDirectories(tmpDir.resolve("plugins"))
-//    val updater = new CommunityIdeaUpdater(tmpDir, logger)
-//    updater.updateIdeaAndPlugins(
-//      BuildInfo.apply("192.6262.9", Keys.IntelliJPlatform.IdeaCommunity, None),
+//  test("IdeaUpdater Integration Test") {
+//    val dumbModeKey = "IdeaUpdater.dumbMode"
+//    sys.props += dumbModeKey -> "idea"
+////    val tmpDir = Files.createTempDirectory("IdeaUpdaterTest")
+//    val tmpDir = Paths.get("/", "tmp", "IdeaUpdater")
+//    Files.createDirectories(tmpDir.resolve("plugins"))
+////    val updater = new CommunityIdeaUpdater(tmpDir, logger)
+////    updater.updateIdeaAndPlugins(
+////      BuildInfo.apply("192.6262.9", Keys.IntelliJPlatform.IdeaCommunity, None),
+////      "mobi.hsz.idea.gitignore".toPlugin :: "org.intellij.scala:2019.2.1144:Nightly".toPlugin :: Nil
+////    )
+////    sbt.IO.delete(tmpDir.toFile)
+//    sys.props -= dumbModeKey
+//  }
+//
+//  test("install mock IDEA test") {
+////    val tmpDir = Files.createTempDirectory(getClass.getSimpleName)
+//    val tmpDir = Paths.get("/tmp/CommunityIdeaUpdaterTest4519926325700609027")
+//    val ideaVersion = "193.6494.35"
+//    val updater = new CommunityUpdater(
+//      tmpDir / ideaVersion,
+//      IDEA_BUILDINFO.copy(buildNumber = ideaVersion),
 //      "mobi.hsz.idea.gitignore".toPlugin :: "org.intellij.scala:2019.2.1144:Nightly".toPlugin :: Nil
 //    )
-//    sbt.IO.delete(tmpDir.toFile)
-    sys.props -= dumbModeKey
-  }
-
-  test("install mock IDEA test") {
-//    val tmpDir = Files.createTempDirectory(getClass.getSimpleName)
-    val tmpDir = Paths.get("/tmp/CommunityIdeaUpdaterTest4519926325700609027")
-    val ideaVersion = "193.6494.35"
-    val updater = new CommunityUpdater(
-      tmpDir / ideaVersion,
-      IDEA_BUILDINFO.copy(buildNumber = ideaVersion),
-      "mobi.hsz.idea.gitignore".toPlugin :: "org.intellij.scala:2019.2.1144:Nightly".toPlugin :: Nil
-    )
-    updater.update()
-  }
+//    updater.update()
+//  }
 
 }
