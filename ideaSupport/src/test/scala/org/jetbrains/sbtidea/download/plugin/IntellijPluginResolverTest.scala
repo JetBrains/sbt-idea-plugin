2166d4eec7efd736f6b48e9d96da29b2a8e7beea
Mikhail Mutcianko <mutcianko.m@gmail.com>
2020-04-01 14:45:16 +0300

    add more plugin resolver tests #57

--- ideaSupport/src/test/scala/org/jetbrains/sbtidea/download/plugin/IntellijPluginResolverTest.scala
+++ ideaSupport/src/test/scala/org/jetbrains/sbtidea/download/plugin/IntellijPluginResolverTest.scala
@@ -11,12 +11,12 @@ class IntellijPluginResolverTest extends IntellijPluginResolverTestBase {
 
   test("plugin resolver doesn't resolve fake plugin") {
     val fakePlugin = "org.myFake.plugin:0.999:trunk".toPlugin
-    val messages = captureLog(new PluginResolver().resolve(fakePlugin) shouldBe empty)
+    val messages = captureLog(new PluginResolver(resolveSettings = fakePlugin.resolveSettings).resolve(fakePlugin) shouldBe empty)
     messages should contain ("Failed to resolve PluginDependency(org.myFake.plugin): null")
   }
 
   test("transitive plugin dependencies are resolved") {
-    val res = new PluginResolver().resolve(pluginC)
+    val res = new PluginResolver(resolveSettings = pluginC.plugin.resolveSettings).resolve(pluginC)
     inside(res) {
       case RemotePluginArtifact(c, _) :: LocalPlugin(a, _, _) :: RemotePluginArtifact(b, _) :: Nil =>
         c shouldBe descriptor2Plugin(pluginC)
@@ -26,10 +26,40 @@ class IntellijPluginResolverTest extends IntellijPluginResolverTestBase {
   }
 
   test("cyclic dependencies are detected") {
-    val resolver = new PluginResolver()
+    val resolver = new PluginResolver(resolveSettings = pluginE.plugin.resolveSettings)
     val (messages, result) = captureLogAndValue(resolver.resolve(pluginE))
     messages shouldBe Seq("Circular plugin dependency detected: PluginDependency(org.E) already processed")
     result.size shouldBe 3
+  }
+
+  test("plugin exclude rules work") {
+    val newResolveSettings = IntellijPlugin.Settings(excludedIds = Set(pluginA.id))
+    val res = new PluginResolver(resolveSettings = newResolveSettings).resolve(pluginC)
+    inside(res) {
+      case RemotePluginArtifact(c, _) :: RemotePluginArtifact(b, _) :: Nil =>
+        c shouldBe descriptor2Plugin(pluginC)
+//        a shouldBe descriptor2Plugin(pluginA)
+        b shouldBe descriptor2Plugin(pluginB)
+    }
+  }
+
+  test("disable transitive resolution") {
+    val newResolveSettings = IntellijPlugin.Settings(transitive = false)
+    val res = new PluginResolver(resolveSettings = newResolveSettings).resolve(pluginC)
+    inside(res) {
+      case RemotePluginArtifact(c, _) :: Nil => c shouldBe descriptor2Plugin(pluginC)
+    }
+  }
+
+  test("disable optional plugins") {
+    val newResolveSettings = IntellijPlugin.Settings(optionalDeps = false)
+    val res = new PluginResolver(resolveSettings = newResolveSettings).resolve(pluginC)
+    inside(res) {
+      case RemotePluginArtifact(c, _) :: RemotePluginArtifact(b, _) :: Nil =>
+        c shouldBe descriptor2Plugin(pluginC)
+        //        a shouldBe descriptor2Plugin(pluginA)
+        b shouldBe descriptor2Plugin(pluginB)
+    }
   }
 
 }
