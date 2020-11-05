6c899ac003512f8b71aff260ec99d21e8ebd70dc
Mikhail Mutcianko <mutcianko.m@gmail.com>
2020-11-06 02:24:08 +0300

    deprecate runner projects generations fix #64

--- ideaSupport/src/main/scala/org/jetbrains/sbtidea/Utils.scala
+++ ideaSupport/src/main/scala/org/jetbrains/sbtidea/Utils.scala
@@ -9,6 +9,11 @@ import sbt.{Def, file, _}
 
 trait Utils { this: Keys.type =>
 
+  /**
+    * Runner projects were required to generate a synthetic IJ module with only IJ platform jars for the run configurations
+    * to work. Since run configuration classpaths are now calculated statically, runner projects are no longer necessary.
+    */
+  @deprecated("runner projects are not required anymore", "3.8.0")
   def createRunnerProject(from: ProjectReference, newProjectName: String = ""): Project = {
     val baseName: String = from match {
       case ProjectRef(_, project) => project
@@ -17,6 +22,7 @@ trait Utils { this: Keys.type =>
       case LocalRootProject       => "plugin"
       case ThisProject            => "plugin"
     }
+    println("Runner projects are deprecated, see createRunnerProject documentation")
     val newName = if (newProjectName.nonEmpty) newProjectName else s"$baseName-runner"
     Project(newName, file(s"target/tools/$newName"))
       .dependsOn(from % Provided)
