76f18837cfa9c7cc62d3281ccfd4d0d54a07e131
Mikhail Mutcianko <mutcianko.m@gmail.com>
2021-01-28 19:41:17 +0300

    extract sources archive name

--- ideaSupport/src/main/scala/org/jetbrains/sbtidea/download/idea/IdeaSources.scala
+++ ideaSupport/src/main/scala/org/jetbrains/sbtidea/download/idea/IdeaSources.scala
@@ -2,11 +2,11 @@ package org.jetbrains.sbtidea.download.idea
 
 import java.net.URL
 import java.nio.file.{Files, Path}
-
 import org.jetbrains.sbtidea.download.FileDownloader
 import org.jetbrains.sbtidea.{PluginLogger, pathToPathExt}
 import sbt._
 import org.jetbrains.sbtidea.download.api._
+import org.jetbrains.sbtidea.download.idea.IdeaSourcesImpl.SOURCES_ZIP
 
 import scala.language.postfixOps
 
@@ -14,10 +14,10 @@ abstract class IdeaSources extends IdeaArtifact {
   override type R = IdeaSources
   override protected def usedInstaller: Installer[IdeaSources] = new Installer[IdeaSources] {
     override def isInstalled(art: IdeaSources)(implicit ctx: InstallContext): Boolean =
-      ctx.baseDirectory / "sources.zip" exists
+      ctx.baseDirectory / SOURCES_ZIP exists
     override def downloadAndInstall(art: IdeaSources)(implicit ctx: InstallContext): Unit = {
       val file = FileDownloader(ctx.baseDirectory.getParent).download(art.dlUrl, optional = true)
-      Files.move(file, ctx.baseDirectory.resolve("sources.zip"))
+      Files.move(file, ctx.baseDirectory.resolve(SOURCES_ZIP))
       PluginLogger.info(s"${caller.buildInfo.edition.name} sources installed")
     }
   }
@@ -28,5 +28,6 @@ class IdeaSourcesImpl(override val caller: AbstractIdeaDependency, dlUrlProvider
 }
 
 object IdeaSourcesImpl {
+  val SOURCES_ZIP = "sources.zip"
   def apply(caller: AbstractIdeaDependency, dlUrlProvider: () => URL): IdeaSourcesImpl = new IdeaSourcesImpl(caller, dlUrlProvider)
 }
