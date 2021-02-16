2857054e8b7fc58f9699cca3a404eaccbd974da2
Mikhail Mutcianko <mutcianko.m@gmail.com>
2021-02-17 01:23:16 +0300

    update locally installed JBR if new dependency is different #88

--- ideaSupport/src/main/scala/org/jetbrains/sbtidea/download/jbr/JbrInstaller.scala
+++ ideaSupport/src/main/scala/org/jetbrains/sbtidea/download/jbr/JbrInstaller.scala
@@ -14,11 +14,35 @@ class JbrInstaller extends Installer[JbrArtifact] {
   import JbrInstaller._
 
   override def isInstalled(art: JbrArtifact)(implicit ctx: InstallContext): Boolean =
-    (ctx.baseDirectory / JBR_DIR_NAME).exists
+    (ctx.baseDirectory / JBR_DIR_NAME).exists && isSameJbr(art)
 
   override def downloadAndInstall(art: JbrArtifact)(implicit ctx: InstallContext): Unit = {
     val file = FileDownloader(ctx.downloadDirectory).download(art.dlUrl)
     install(file)
+  }
+
+  private def isSameJbr(art: JbrArtifact)(implicit ctx: InstallContext): Boolean = {
+    val releaseFile = ctx.baseDirectory / JBR_DIR_NAME / "release"
+    val props = new Properties()
+    try {
+      val jbrInfo = art.caller.jbrInfo
+      using(releaseFile.inputStream)(props.load)
+      props
+        .getProperty("IMPLEMENTOR_VERSION")
+        .lift2Option
+        .exists(value => {
+          val sameJbr =
+             value.contains(jbrInfo.major.replace('_', '.')) && // release file has dot major version separators
+             value.contains(jbrInfo.minor) &&
+             value.contains(jbrInfo.kind.replace("jbr_", "")) // release file has no "jbr_" prefix
+          if (!sameJbr) log.info(s"New JBR is different from installed: $jbrInfo != $value")
+          sameJbr
+        })
+    } catch {
+      case e: Exception =>
+        log.error(s"Failed to check locally installed JBR version(assuming true): ${e.getMessage}")
+        true
+    }
   }
 
   private[jbr] def install(dist: Path)(implicit ctx: InstallContext): Unit = {
