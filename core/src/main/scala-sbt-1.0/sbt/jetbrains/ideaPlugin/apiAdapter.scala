a4eaae3777af476d3baa26ab7669891f9d9bcbc0
Mikhail Mutcianko <mutcianko.m@gmail.com>
2021-02-26 19:20:04 +0300

    upgrade sbt to 1.4.5

--- core/src/main/scala-sbt-1.0/sbt/jetbrains/ideaPlugin/apiAdapter.scala
+++ core/src/main/scala-sbt-1.0/sbt/jetbrains/ideaPlugin/apiAdapter.scala
@@ -9,34 +9,9 @@ object apiAdapter {
   val Using = sbt.io.Using
   type BuildDependencies = sbt.internal.BuildDependencies
 
-  def projectJarName(project: sbt.Project): String = s"${project.id}.jar"
   def extractAffectedFiles(result: CompileResult): Seq[sbt.File] = Seq.empty
 
-  object SbtCompilationBackCompat {
-    type Analysis         = sbt.internal.inc.Analysis
-    type Relations        = sbt.internal.inc.Relations
-    type CompileResult    = xsbti.compile.CompileResult
-    type CompileAnalysis  = xsbti.compile.CompileAnalysis
-    type PreviousResult   = xsbti.compile.PreviousResult
-    type ClassFileManager = xsbti.compile.ClassFileManager
-    type IncOptions       = xsbti.compile.IncOptions
-
-    val Analysis = sbt.internal.inc.Analysis
-
-    implicit class CompileResultExt(val result: PreviousResult) extends AnyVal {
-      def getAnalysis: Optional[CompileAnalysis] = result.analysis()
-    }
-
-    implicit class IncOptionsExt(val options: IncOptions) extends AnyVal {
-      def withClassfileManager(manager: ClassFileManager): IncOptions =
-        options.withExternalHooks(options.externalHooks().withExternalClassFileManager(manager))
-    }
-
-    object PreviousResult {
-      def empty(): PreviousResult =
-        xsbti.compile.PreviousResult.create(Optional.empty(), Optional.empty())
-    }
-  }
+  def projectJarName(project: sbt.Project): String = s"${project.id}.jar"
 
   // / method is missing because it's already implemented in sbt 1.3 PathOps
   final class PathExt(val path: Path) extends AnyVal {
