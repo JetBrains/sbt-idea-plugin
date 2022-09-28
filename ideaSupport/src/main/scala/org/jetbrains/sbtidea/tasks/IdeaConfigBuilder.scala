001233e0d342c4f69461d0d7f76dc3ee821dbe3d
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2022-09-28 16:08:34 +0200

    Revert "Recursively look for IntelliJ Platform jars to put on the classpath"
    This reverts commit 8befaef74745f49512986e1166c976e75d5b5361.

--- ideaSupport/src/main/scala/org/jetbrains/sbtidea/tasks/IdeaConfigBuilder.scala
+++ ideaSupport/src/main/scala/org/jetbrains/sbtidea/tasks/IdeaConfigBuilder.scala
@@ -7,12 +7,10 @@ import org.jetbrains.sbtidea.{ClasspathStrategy, pathToPathExt, PluginLogger => 
 import sbt._
 
 import java.io.File
-import java.nio.file.{Files, Path, Paths}
+import java.nio.file.{Path, Paths}
 import java.util.regex.Pattern
-import java.util.stream.Collectors
 import scala.annotation.tailrec
 import scala.collection.mutable
-import scala.jdk.CollectionConverters._
 import scala.math.Ordering.Implicits.infixOrderingOps
 
 /**
@@ -195,16 +193,15 @@ class IdeaConfigBuilder(moduleName: String,
 
   }
 
-  private def intellijPlatformJarsClasspath: Seq[String] =
+  private def intellijPlatformJarsClasspath: Seq[String] = {
     //NOTE: see also filtering in org.jetbrains.sbtidea.Init.projectSettings
-    Files.walk(intellijPlatformJarsFolder.toPath)
-      .filter { p =>
-        val fileName = p.getFileName.toString
+    intellijPlatformJarsFolder
+      .listFiles((_, fileName: String) => {
         fileName.endsWith(".jar") && fileName != "junit.jar" && fileName != "junit4.jar"
-      }
-      .map[String](_.toString)
-      .collect(Collectors.toList[String])
-      .asScala
+      })
+      .map(_.getPath)
+      .toSeq
+  }
 
   private def pluginClasspathPattern(pluginPath: File): String =
     if (pluginPath.isDirectory)
