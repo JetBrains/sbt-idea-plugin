5c516e84c7c16f4d22cc90dc7f273cdabd3df720
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2024-01-19 19:10:29 +0100

    fix inspections: "Redundant character escape ... in RegExp"

--- core/src/main/scala/org/jetbrains/sbtidea/package.scala
+++ core/src/main/scala/org/jetbrains/sbtidea/package.scala
@@ -14,7 +14,7 @@ package object sbtidea {
     def removeSpaces: String = str.replaceAll("\\s+", "")
     def xmlQuote: String   = s"&quot;$str&quot;"
     def isValidFileName: Boolean =
-      str.matches("\\A(?!(?:COM[0-9]|CON|LPT[0-9]|NUL|PRN|AUX|com[0-9]|con|lpt[0-9]|nul|prn|aux)|[\\s\\.])[^\\\\\\/:*\"?<>|]{1,254}\\z")
+      str.matches("\\A(?!(?:COM[0-9]|CON|LPT[0-9]|NUL|PRN|AUX|com[0-9]|con|lpt[0-9]|nul|prn|aux)|[\\s.])[^\\\\/:*\"?<>|]{1,254}\\z")
   }
 
   implicit class IteratorExt[A](private val delegate: Iterator[A]) extends AnyVal {
