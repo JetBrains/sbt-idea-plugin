392ec9bff4db520d2448f1a17d1cb34218d61321
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2024-01-19 19:15:45 +0100

    fix inspections: "Java language level migration aids"

--- ideaSupport/src/main/scala/org/jetbrains/sbtidea/download/VersionComparatorUtil.java
+++ ideaSupport/src/main/scala/org/jetbrains/sbtidea/download/VersionComparatorUtil.java
@@ -89,7 +89,7 @@ public class VersionComparatorUtil {
 
   private static List<String> splitVersionString(final String ver) {
     StringTokenizer st = new StringTokenizer(ver.trim(), "()._-;:/, +~");
-    List<String> result = new ArrayList<String>();
+    List<String> result = new ArrayList<>();
 
     while (st.hasMoreTokens()) {
       final Matcher matcher = WORDS_SPLITTER.matcher(st.nextToken());
