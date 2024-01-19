b1eebcf5fffb2f119ddbf12ec1c38ca06c1f3976
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2024-01-19 19:18:32 +0100

    fix inspections: AutoCloseable used without 'try'-with-resources

--- ideaSupport/src/main/scala/org/jetbrains/sbtidea/download/NioUtils.java
+++ ideaSupport/src/main/scala/org/jetbrains/sbtidea/download/NioUtils.java
@@ -3,14 +3,16 @@ package org.jetbrains.sbtidea.download;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.Comparator;
+import java.util.stream.Stream;
 
 public class NioUtils {
     public static void delete(Path path) throws IOException {
         if (!path.toFile().exists()) return;
 
-        Files.walk(path)
-                .sorted(Comparator.reverseOrder())
-                .forEach(NioUtils::silentDelete);
+        try(Stream<Path> stream = Files.walk(path)) {
+            stream.sorted(Comparator.reverseOrder())
+                    .forEach(NioUtils::silentDelete);
+        }
     }
 
     private static void silentDelete(Path path) {
