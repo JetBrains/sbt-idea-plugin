879a027cb525869d033a27389ed810b57cda58a3
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2024-01-22 15:59:39 +0100

    Revert "fix inspections: "Duplicated code fragment""
    This reverts commit d3e09e0c493a7ba69f678d9fc47548ddafca7167.

--- packaging/src/main/scala/org/jetbrains/sbtidea/packaging/apiAdapter.scala
+++ packaging/src/main/scala/org/jetbrains/sbtidea/packaging/apiAdapter.scala
@@ -2,14 +2,31 @@ package org.jetbrains.sbtidea.packaging
 
 import org.jetbrains.sbtidea.packaging.PackagingKeys.*
 import org.jetbrains.sbtidea.packaging.structure.sbtImpl.SbtPackageProjectData
-import sbt.{Def, *}
+import sbt.*
 import sbt.Keys.*
 
 object apiAdapter {
 
-  def dumpDependencyStructureOffline: Def.Initialize[Task[SbtPackageProjectData]] = dumpDependencyStructure
+  def dumpDependencyStructureOffline = Def.task {
+    SbtPackageProjectData(
+      thisProjectRef.value,
+      name.in(thisProjectRef).value,
+      managedClasspath.in(Compile).value,
+      libraryDependencies.in(Compile).value,
+      packageAdditionalProjects.value,
+      packageAssembleLibraries.value,
+      productDirectories.in(Compile).value,
+      updateFull.value,
+      packageLibraryMappings.value,
+      packageLibraryBaseDir.value,
+      packageFileMappings.value,
+      packageMethod.value,
+      shadePatterns.value,
+      pathExcludeFilter.value
+    )
+  }
 
-  def dumpDependencyStructure: Def.Initialize[Task[SbtPackageProjectData]] = Def.task {
+  def dumpDependencyStructure = Def.task {
     SbtPackageProjectData(
       thisProjectRef.value,
       name.in(thisProjectRef).value,
@@ -27,4 +44,5 @@ object apiAdapter {
       pathExcludeFilter.value
     )
   }
+
 }
