6c4ca70e65930005808b22de60ac82ac1255ab4f
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2024-01-22 15:21:08 +0100

    add type annotation to PackagingKeys public declarations

--- packaging/src/main/scala/org/jetbrains/sbtidea/packaging/PackagingKeys.scala
+++ packaging/src/main/scala/org/jetbrains/sbtidea/packaging/PackagingKeys.scala
@@ -6,36 +6,40 @@ import sbt.*
 
 object PackagingKeys extends PackagingKeysInit with PackagingDefs {
 
-  /* Settings */
+  //=========================================================
+  // Settings
+  //=========================================================
 
-  lazy val packageMethod = settingKey[PackagingMethod](
+  lazy val packageMethod: SettingKey[PackagingKeys.PackagingMethod] = settingKey[PackagingMethod](
     "What kind of artifact to produce from given project")
 
-  lazy val packageAdditionalProjects = settingKey[Seq[Project]](
+  lazy val packageAdditionalProjects: SettingKey[Seq[Project]] = settingKey[Seq[Project]](
     "Projects to package alongside current, without adding classpath dependencies")
   
-  lazy val packageLibraryMappings = settingKey[Seq[(ModuleID, Option[String])]](
+  lazy val packageLibraryMappings: SettingKey[Seq[(sbt.ModuleID, Option[String])]] = settingKey[Seq[(ModuleID, Option[String])]](
     "Overrides for library mappings in artifact")
 
-  lazy val packageLibraryBaseDir = settingKey[File](
+  lazy val packageLibraryBaseDir: SettingKey[sbt.File] = settingKey[File](
     "Directory to place library dependencies into. *Relative* to the artifact output dir")
 
-  lazy val packageAssembleLibraries = settingKey[Boolean](
+  lazy val packageAssembleLibraries: SettingKey[Boolean] = settingKey[Boolean](
     "Should the project library dependencies be merged inside the project artifact")
 
-  lazy val packageOutputDir = settingKey[File](
+  lazy val packageOutputDir: SettingKey[sbt.File] = settingKey[File](
     "Folder to write artifact to")
 
-  lazy val packageArtifactZipFile = settingKey[File](
+  lazy val packageArtifactZipFile: SettingKey[sbt.File] = settingKey[File](
     "Target file for packaging with packageArtifactZip task")
 
-  lazy val shadePatterns = settingKey[Seq[ShadePattern]](
+  lazy val shadePatterns: SettingKey[Seq[PackagingKeys.ShadePattern]] = settingKey[Seq[ShadePattern]](
     "Class renaming patterns in jars")
 
-  lazy val pathExcludeFilter = settingKey[ExcludeFilter](
+  lazy val pathExcludeFilter: SettingKey[ExcludeFilter] = settingKey[ExcludeFilter](
     "Paths to exclude within merged jars")
 
-  /* Tasks */
+  //=========================================================
+  // Tasks
+  //=========================================================
 
   lazy val packageFileMappings = taskKey[Seq[(File, String)]](
     "Extra files or directories to include into the artifact")
@@ -53,9 +57,18 @@ object PackagingKeys extends PackagingKeysInit with PackagingDefs {
     "Find and debug library mappings by library name")
 
 
-  lazy val dumpDependencyStructure        = taskKey[SbtPackageProjectData]("").withRank(sbt.KeyRanks.Invisible)
-  lazy val dumpDependencyStructureOffline = taskKey[SbtPackageProjectData]("").withRank(sbt.KeyRanks.Invisible)
-  lazy val packageMappings                = taskKey[Mappings]("").withRank(sbt.KeyRanks.Invisible)
-  lazy val packageMappingsOffline         = taskKey[Mappings]("").withRank(sbt.KeyRanks.Invisible)
-  lazy val doPackageArtifactZip           = taskKey[File]("").withRank(sbt.KeyRanks.Invisible)
+  lazy val dumpDependencyStructure: TaskKey[SbtPackageProjectData] =
+    taskKey[SbtPackageProjectData]("").withRank(sbt.KeyRanks.Invisible)
+
+  lazy val dumpDependencyStructureOffline: TaskKey[SbtPackageProjectData] =
+    taskKey[SbtPackageProjectData]("").withRank(sbt.KeyRanks.Invisible)
+
+  lazy val packageMappings: TaskKey[Mappings] =
+    taskKey[Mappings]("").withRank(sbt.KeyRanks.Invisible)
+
+  lazy val packageMappingsOffline: TaskKey[Mappings] =
+    taskKey[Mappings]("").withRank(sbt.KeyRanks.Invisible)
+
+  lazy val doPackageArtifactZip: TaskKey[sbt.File] =
+    taskKey[File]("").withRank(sbt.KeyRanks.Invisible)
 }
