f7a74bc313a6fc78e10c6923bd8add7b2ed1ddc2
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2024-01-22 15:30:30 +0100

    added back `val autoImport` in sbt AutoPlugin implementations
    It was removed in this commit: e7ed7426
    and broke plugin usage (the keys defined in sbtidea.Keys were not detected in Scala Plugin repo)

--- ideaSupport/src/main/scala/org/jetbrains/sbtidea/SbtIdeaPlugin.scala
+++ ideaSupport/src/main/scala/org/jetbrains/sbtidea/SbtIdeaPlugin.scala
@@ -1,10 +1,13 @@
 package org.jetbrains.sbtidea
 
+import org.jetbrains.sbtidea
 import org.jetbrains.sbtidea.packaging.PackagingPlugin
 import org.jetbrains.sbtidea.tasks.structure.render.ProjectStructureVisualizerPlugin
 import sbt.{AutoPlugin, Def, Setting}
 
 abstract class AbstractSbtIdeaPlugin extends AutoPlugin {
+  val autoImport: Keys.type = sbtidea.Keys
+
   override def requires = PackagingPlugin && ProjectStructureVisualizerPlugin
   override def globalSettings: Seq[Def.Setting[?]] = Keys.globalSettings
   override def buildSettings: Seq[Setting[?]]   = Keys.buildSettings
