eb397b5311658f75953c9f387dc5f630497de4e1
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2024-05-14 18:20:00 +0200

    project extractor: extract common utilities to check if a library is scala 2/3 library #126

--- core/src/main/scala/org/jetbrains/sbtidea/structure/sbtImpl/package.scala
+++ core/src/main/scala/org/jetbrains/sbtidea/structure/sbtImpl/package.scala
@@ -29,4 +29,29 @@ package object sbtImpl {
   }
 
   class SbtProjectExtractException(message: String) extends Exception(message)
+
+  private def isScala2Library(lib: ModuleID): Boolean = lib.name == "scala-library"
+  private def isScala3Library(lib: ModuleID): Boolean = lib.name == "scala3-library_3"
+  def isScalaLibrary(lib: ModuleID): Boolean = isScala2Library(lib) || isScala3Library(lib)
+
+  /**
+   * @return Scala 3 library if it's present<br>
+    *         orScala 2 library if its present<br>
+    *         or None otherwise
+    */
+  def detectMainScalaLibrary(dependencies: Seq[ModuleID]): Option[ModuleID] = {
+    val scalaLibraries = dependencies.filter(isScalaLibrary)
+    if (scalaLibraries.isEmpty) None else Some(scalaLibraries.maxBy(isScala3Library))
+  }
+
+  def detectScalaVersion(projectData: CommonSbtProjectData): ProjectScalaVersionImpl = {
+    val mainScalaLibraryFromClasspath = detectMainScalaLibrary(projectData.cp.flatMap(_.metadata.get(sbt.Keys.moduleID.key)))
+
+    val mainScalaLibrary = if (mainScalaLibraryFromClasspath.nonEmpty)
+      mainScalaLibraryFromClasspath
+    else
+      detectMainScalaLibrary(projectData.definedDeps)
+
+    ProjectScalaVersionImpl(mainScalaLibrary)
+  }
 }
