05e7ef665f59c57ed3ff91ed782a1a2219706388
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2024-06-18 11:38:38 +0200

    [prod/test sources] change the generation of project names for MappingMetaData

--- packaging/src/main/scala/org/jetbrains/sbtidea/packaging/package.scala
+++ packaging/src/main/scala/org/jetbrains/sbtidea/packaging/package.scala
@@ -6,4 +6,8 @@ package object packaging {
 
   implicit def MappingOrder[A <: Mapping]: Ordering[A] = Ordering.by(x => x.from -> x.to) // order by target jar file
 
+  private[packaging] val ProdTestSourcesKey = "separate.prod.test.sources.enabled"
+
+  def hasProdTestSeparationEnabled: Boolean =
+    sys.props.get(ProdTestSourcesKey).exists(_.toBoolean)
 }
