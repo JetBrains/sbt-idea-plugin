857575a927e943727d74af0efe77634293b7f879
Dmitrii Naumenko <dmitrii.naumenko@jetbrains.com>
2026-06-02 09:32:32 +0200

    Cache published sbt-idea-plugin version in a lazy val between tests run in same JVM instance

--- testUtils/src/test/scala/org/jetbrains/sbtidea/testUtils/CurrentEnvironmentUtils.scala
+++ testUtils/src/test/scala/org/jetbrains/sbtidea/testUtils/CurrentEnvironmentUtils.scala
@@ -3,33 +3,39 @@ package org.jetbrains.sbtidea.testUtils
 import org.jetbrains.sbtidea.testUtils.SbtProjectFilesUtils.IoMode.PrintAndCollectOutput
 
 import java.io.File
+import scala.util.Try
 
 object CurrentEnvironmentUtils {
 
   val CurrentWorkingDir: File = new File(".").getCanonicalFile
   val CurrentJavaHome: String = System.getProperty("java.home")
 
+  // Cache the negative result (exception) as well and rethrow the exception
+  private lazy val PublishedSbtIdeaPluginVersion: Try[String] =
+    Try(publishCurrentSbtIdeaPluginToLocalRepoAndGetVersionsUncached)
+
   /**
-   * @return version of the locally-published plugin
+   * @return version of the locally published plugin
    */
   //noinspection ScalaUnusedSymbol
-  def publishCurrentSbtIdeaPluginToLocalRepoAndGetVersions: String = {
+  def publishCurrentSbtIdeaPluginToLocalRepoAndGetVersions: String = PublishedSbtIdeaPluginVersion.get
+
+  private def publishCurrentSbtIdeaPluginToLocalRepoAndGetVersionsUncached: String = {
     println("Publishing sbt-idea-plugin to local repository and getting it's version")
 
     val sbtArguments = Seq(
       // Keep the temporary failure as a commented command inside this Seq.
       // If it is needed again, uncomment this line and comment out the real command below.
       // This avoids accidentally passing two positional command arguments to runSbtProcess.
-      // TODO: remove this line
-//      "codexIntentionalFailureForDebugOutput",
       "compile ; publishLocal ; show core / version",
     )
 
-    val outputLines = SbtProjectFilesUtils.runSbtProcess(
+    val sbtResult = SbtProjectFilesUtils.runSbtProcess(
       sbtArguments,
       CurrentWorkingDir,
       ioMode = PrintAndCollectOutput,
-    ).outputLines.get
+    )
+    val outputLines = sbtResult.outputLines.get
 
     val infoLines = outputLines
       .map(_.trim)
